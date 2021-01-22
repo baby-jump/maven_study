c3afa8cfdded08d9e6456f4fb7a69cd2fa0ff35e
the_rest_of_life <18642967872@163.com>
2021-01-23 05:45:00 +0800

    测试

---
+++ src/main/java/doc/Demo.java
@@ -0,0 +1,12 @@
+package doc;
+
+public class Demo {
+    /**
+     * @param name 指定的用户名字
+     * @return 含有问候语的字符串
+     */
+    public  String sayHello(String name){
+        return "你好"+name;
+    }
+
+}
