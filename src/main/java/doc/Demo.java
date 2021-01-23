d89b745015316d00e189b23b50f6170ae7f616f0
the_rest_of_life <18642967872@163.com>
2021-01-23 09:50:49 +0800

    测试1

--- src/main/java/doc/Demo.java
+++ src/main/java/doc/Demo.java
@@ -5,6 +5,13 @@ public class Demo {
      * @param name 指定的用户名字
      * @return 含有问候语的字符串
      */
+    //add   文件放到缓存区中
+    //commit    提交到本地库
+    //push      上传到Github远程仓库
+    //pull      拉取我们的文件
+
+    //文件  - >  .java .class .txt .md .word
+    //目录  - >  文件夹
     public  String sayHello(String name){
         return "你好"+name;
     }
